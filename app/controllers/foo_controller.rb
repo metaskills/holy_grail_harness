5fbbaa4b66c507139220495404427712eb611c28
Ken Collins <ken@metaskills.net>
2013-01-17 22:12:57 -0500

    Sorry for checking in all that Foo stuff.

--- app/controllers/foo_controller.rb
+++
@@ -1,4 +0,0 @@
-class FooController < ApplicationController
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 HolyGrailHarness::Application.routes.draw do
   
-  get "foo/index"
-
   root :to => 'application#index'
 
   
